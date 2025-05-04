cc1c98b4543acde0f2a57a1f7863816ac1d92eab
poojito <poojittummalapalli@gmail.com>
2025-05-04 16:23:27 -0400

    just committing what I have been working on the past week extending the typedefs

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,10 @@
 import {ApolloServer} from '@apollo/server';
 import {startStandaloneServer} from '@apollo/server/standalone';
 
-import {typeDefs} from './typeDefs.js';
+import {typeDefs} from './typedefs.js';
 import {resolvers} from './resolvers.js';
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+const server = new ApolloServer({typeDefs, resolvers});
 
 const {url} = await startStandaloneServer(server, {
   listen: {port: 4000}
--- backend/typedefs.js
+++ backend/typedefs.js
@@ -1,124 +1,232 @@
-export const typeDefs = `#graphql
-  ####################
-  # Core Types
-  ####################
-  type User {
-    _id: String!
-    name: String!
-    bodyWeight: Float!
-    workouts: [Workout!]!
-    calorieEntries: [CalorieEntry!]!
-    bodyWeightEntries: [BodyWeightEntry!]!
-  }
+import gql from 'graphql-tag';
+export const typeDefs = gql`
+ ####################
+   # Core Types
+   ####################
+   type User {
+     _id: String!
+     name: String!
+     bodyWeight: Float!
+     workouts: [Workout!]!
+     calorieEntries: [CalorieEntry!]!
+     bodyWeightEntries: [BodyWeightEntry!]!
+   }
+ 
+   type Workout {
+     _id: String!
+     name: String!
+     userId: User!
+     date: String!
+     exercises: [Exercise!]!
+   }
+ 
+   type Exercise {
+     _id: String!
+     name: String!
+     sets: [Set!]!
+     workout: Workout!
+   }
 
-  type Workout {
-    _id: String!
-    name: String!
-    user: User!
+   type TemplateExercise {
+      name: String!
+      sets: [Set!]!
+    }
+ 
+   type Set {
+     weight: Float!
+     reps: Int!
+     rir: Int!
+   }
+ 
+   type CalorieEntry {
+     _id: String!
+     userId: User!
+     food: String!
+     calories: Int!
+     protein: Float!
+     carbs: Float!
+     fats: Float!
+     date: String!
+   }
+ 
+   type BodyWeightEntry {
+     _id: String!
+     userId: User!
+     weight: Float!
+     date: String!
+   }
+ 
+   ####################
+   # Analytics Types
+   ####################
+   type ProgressionSuggestion {
+     exerciseId: String!
+     suggestedWeight: Float
+     suggestedReps: Int
+     rationale: String
+   }
+ 
+   type ExerciseVariation {
+     originalExercise: Exercise!
+     alternatives: [Exercise!]!
+   }
+ 
+   type BodyWeightGraphPoint {
+     date: String!
+     weight: Float!
+   }
+ 
+   type CalorieGraphPoint {
+     date: String!
+     calories: Int!
+     protein: Float!
+     carbs: Float!
+     fats: Float!
+   }
+ 
+   type PersonalRecord {
+     exerciseName: String!
+     maxWeight: Float!
+     maxReps: Int!
+     dateAchieved: String!
+   }
+ 
+   ####################
+   # Goals
+   ####################
+   type UserGoals {
+     dailyCalorieTarget: Int
+     proteinTarget: Float
+     carbTarget: Float
+     fatTarget: Float
+     weightGoal: Float
+     goalType: String
+   }
+ 
+   input UserGoalsInput {
+     dailyCalorieTarget: Int
+     proteinTarget: Float
+     carbTarget: Float
+     fatTarget: Float
+     weightGoal: Float
+     goalType: String
+   }
+ 
+   ####################
+   # Templates
+   ####################
+   type WorkoutTemplate {
+     _id: String!
+     name: String!
+     exercises: [TemplateExercise!]!
+   }
+ 
+   input ExerciseInput {
+     name: String!
+     sets: [SetInput!]!
+   }
+ 
+   input SetInput {
+     weight: Float!
+     reps: Int!
+     rir: Int!
+   }
+ 
+   ####################
+   # Queries
+   ####################
+   type Query {
+     users: [User]
+     getUserById(_id: String!): User
+ 
+     workouts(userId: String!): [Workout]
+     getWorkoutById(_id: String!): Workout
+     exercises(workoutId: String!): [Exercise]
+ 
+     calorieEntries(userId: String!): [CalorieEntry]
+     bodyWeightEntries(userId: String!): [BodyWeightEntry]
+ 
+ 
+     getProgressionSuggestions(userId: String!, workoutId: String!): [ProgressionSuggestion!]!
+     getExerciseVariations(exerciseName: String!): ExerciseVariation!
+ 
+     getBodyWeightGraphData(userId: String!, startDate: String!, endDate: String!): [BodyWeightGraphPoint!]!
+     getCalorieGraphData(userId: String!, startDate: String!, endDate: String!): [CalorieGraphPoint!]!
+ 
+     getUserGoals(userId: String!): UserGoals
+ 
+     getWorkoutTemplates(userId: String!): [WorkoutTemplate!]!
+     getScheduledWorkouts(userId: String!, date: String!): [Workout!]!
+ 
+     getPersonalRecords(userId: String!): [PersonalRecord!]!
+   }
+####################
+# Mutations
+####################
+type Mutation {
+  addUser(name: String!, bodyWeight: Float!): User
+  editUser(_id: String!, name: String, bodyWeight: Float): User
+  removeUser(_id: String!): User
+
+  logWorkout(userId: String!, name: String!): Workout
+
+  addExercise(workoutId: String!, name: String!, sets: [SetInput!]!): Exercise
+  editExercise(_id: String!, name: String, sets: [SetInput!]): Exercise
+  removeExercise(_id: String!): Exercise
+
+  addSet(exerciseId: String!, weight: Float!, reps: Int!, rir: Int!): Set
+  editSet(exerciseId: String!, setIndex: Int!, weight: Float, reps: Int, rir: Int): Set
+  removeSet(exerciseId: String!, setIndex: Int!): Set
+
+  addCalorieEntry(
+    userId: String!,
+    food: String!,
+    calories: Int!,
+    protein: Float!,
+    carbs: Float!,
+    fats: Float!,
     date: String!
-    exercises: [Exercise!]!
-  }
+  ): CalorieEntry
 
-  type Exercise {
-    _id: String!
+  editCalorieEntry(
+    _id: String!,
+    food: String,
+    calories: Int,
+    protein: Float,
+    carbs: Float,
+    fats: Float,
+    date: String
+  ): CalorieEntry
+
+  removeCalorieEntry(_id: String!): CalorieEntry
+
+  addBodyWeightEntry(
+    userId: String!,
+    weight: Float!,
+    date: String!
+  ): BodyWeightEntry
+
+  editBodyWeightEntry(
+    _id: String!,
+    weight: Float,
+    date: String
+  ): BodyWeightEntry
+
+  removeBodyWeightEntry(_id: String!): BodyWeightEntry
+
+  addWorkout(
+    userId: ID!
     name: String!
-    sets: [Set!]!
-    workout: Workout!
-  }
-
-  type Set {
-    weight: Float!
-    reps: Int!
-    rir: Int!
-  }
-
-  type CalorieEntry {
-    _id: String!
-    user: User!
-    food: String!
-    calories: Int!
-    protein: Float!
-    carbs: Float!
-    fats: Float!
     date: String!
-  }
+  ): Workout!
 
-  type BodyWeightEntry {
-    _id: String!
-    user: User!
-    weight: Float!
-    date: String!
-  }
+  editWorkout(
+    _id: ID!
+    name: String
+    date: String
+  ): Workout!
 
-  ####################
-  # Analytics Types
-  ####################
-  type ProgressionSuggestion {
-    exerciseId: String!
-    suggestedWeight: Float
-    suggestedReps: Int
-    rationale: String
-  }
-
-  type ExerciseVariation {
-    originalExercise: Exercise!
-    alternatives: [Exercise!]!
-  }
-
-  type BodyWeightGraphPoint {
-    date: String!
-    weight: Float!
-  }
-
-  type CalorieGraphPoint {
-    date: String!
-    calories: Int!
-    protein: Float!
-    carbs: Float!
-    fats: Float!
-  }
-
-  type PersonalRecord {
-    exerciseName: String!
-    maxWeight: Float!
-    maxReps: Int!
-    dateAchieved: String!
-  }
-
-  ####################
-  # Goals & Friends
-  ####################
-  type UserGoals {
-    dailyCalorieTarget: Int
-    proteinTarget: Float
-    carbTarget: Float
-    fatTarget: Float
-    weightGoal: Float
-    goalType: String
-  }
-
-  input UserGoalsInput {
-    dailyCalorieTarget: Int
-    proteinTarget: Float
-    carbTarget: Float
-    fatTarget: Float
-    weightGoal: Float
-    goalType: String
-  }
-
-  ####################
-  # Templates
-  ####################
-  type WorkoutTemplate {
-    _id: String!
-    name: String!
-    exercises: [ExerciseInput!]!
-  }
-
-  input ExerciseInput {
-    name: String!
-    sets: [SetInput!]!
-  }
+  removeWorkout(_id: ID!): Workout!
 
   input SetInput {
     weight: Float!
@@ -126,52 +234,36 @@ export const typeDefs = `#graphql
     rir: Int!
   }
 
-  ####################
-  # Queries
-  ####################
-  type Query {
-    users: [User]
-    getUserById(_id: String!): User
+  addExercise(
+    workoutId: ID!
+    name: String!
+    sets: [SetInput!]!
+  ): Exercise!
 
-    workouts(userId: String!): [Workout]
-    getWorkoutById(_id: String!): Workout
-    exercises(workoutId: String!): [Exercise]
+  editExercise(
+    _id: ID!
+    name: String
+    sets: [SetInput!]
+  ): Exercise!
 
-    calorieEntries(userId: String!): [CalorieEntry]
-    bodyWeightEntries(userId: String!): [BodyWeightEntry]
+  removeExercise(_id: ID!): Exercise!
 
 
-    getProgressionSuggestions(userId: String!, workoutId: String!): [ProgressionSuggestion!]!
-    getExerciseVariations(exerciseName: String!): ExerciseVariation!
 
-    getWeightGraphData(userId: String!, startDate: String!, endDate: String!): [WeightGraphPoint!]!
-    getCalorieGraphData(userId: String!, startDate: String!, endDate: String!): [CalorieGraphPoint!]!
+  setUserGoals(
+    userId: String!,
+    dailyCalorieTarget: Int,
+    proteinTarget: Float,
+    carbTarget: Float,
+    fatTarget: Float,
+    weightGoal: Float,
+    goalType: String
+  ): UserGoals
 
-    getUserGoals(userId: String!): UserGoals
+  addFriend(userId: String!, friendId: String!): User
+  removeFriend(userId: String!, friendId: String!): User
 
-    getWorkoutTemplates(userId: String!): [WorkoutTemplate!]!
-    getScheduledWorkouts(userId: String!, date: String!): [Workout!]!
-
-    getPersonalRecords(userId: String!): [PersonalRecord!]!
-  }
-
-  ####################
-  # Mutations
-  ####################
-  type Mutation {
-    addUser(name: String!, bodyWeight: Float!): User
-
-    logWorkout(userId: String!, name: String!): Workout
-    addExercise(workoutId: String!, name: String!, sets: [SetInput!]!): Exercise
-
-    logCalorieEntry(userId: String!, food: String!, calories: Int!, protein: Float!, carbs: Float!, fats: Float!): CalorieEntry
-    logBodyWeightEntry(userId: String!, weight: Float!, date: String!): BodyWeightEntry
-
-    setUserGoals( userId: String!, dailyCalorieTarget: Int, proteinTarget: Float, carbTarget: Float, fatTarget: Float, weightGoal: Float, goalType: String): UserGoals
-    addFriend(userId: String!, friendId: String!): User
-    removeFriend(userId: String!, friendId: String!): User
-
-    createWorkoutTemplate(userId: String!, name: String!, exercises: [ExerciseInput!]!): WorkoutTemplate
-    scheduleWorkout(userId: String!, date: String!, templateId: String!): Workout
-  }
+  createWorkoutTemplate(userId: String!, name: String!, exercises: [ExerciseInput!]!): WorkoutTemplate
+  scheduleWorkout(userId: String!, date: String!, templateId: String!): Workout
+}
 `;
