cfae3318cc725bdaa8b0bb728d83563ea36111eb
tkwonster <111104549+tkwonster@users.noreply.github.com>
2025-05-11 22:37:20 -0400

    Update queries.js: implemented GET_USER_PROFILE (subject to change)

--- frontend/graphql/queries.js
+++ frontend/graphql/queries.js
@@ -1,3 +1,5 @@
+//This file defines all GraphQL queries used across the app - this page is utilized by the pages
+
 import { gql } from "@apollo/client";
 
 export const GET_CALORIE_ENTRIES = gql`
@@ -10,6 +12,18 @@ export const GET_CALORIE_ENTRIES = gql`
       carbs
       fats
       date
+    }
+  }
+`;
+
+//Thomas Kwon- Do not change (yet)
+//Retrieves first name, last name, and weight based on Firebase UID
+export const GET_USER_PROFILE = gql`
+  query GetUserProfile($userId: String!) {
+    userProfile(userId: $userId) {
+      firstName
+      lastName
+      weight
     }
   }
 `;
